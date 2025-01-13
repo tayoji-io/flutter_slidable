177c13f995b72c69f489c1e3e439dafdab6574a8
tayoji-io <tayoji.io@outlook.com>
2025-01-13 15:37:29 +0800

    #优化# 适配3.27

--- lib/src/notifications_old.dart
+++ lib/src/notifications_old.dart
@@ -85,7 +85,7 @@ class SlidableRatioNotification extends SlidableNotification {
   }
 
   @override
-  int get hashCode => Object.hash(tag, ratio);
+  int get hashCode => Object.hashAll([tag, ratio]);
 
   @override
   String toString() => 'SlidableRatioNotification(tag: $tag, ratio: $ratio)';
